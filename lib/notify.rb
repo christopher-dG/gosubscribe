8e3cd222e994e1d28b474721ae29691e4cbc2571
Chris de Graaf <chrisadegraaf@gmail.com>
2017-04-27 00:29:19 +0000

    Change notify script map source

--- lib/notify.rb
+++ lib/notify.rb
@@ -8,7 +8,8 @@ def notify(map, mapper)
   DB.exec(cmd).each do |data|
     user = User.new(data).to_discord_user
     begin
-      user.pm("New map by #{mapper.username}: https://osu.ppy.sh/b/#{map['beatmap_id']}")
+      status = map['approved'] == '1' ? 'ranked' : 'qualified'
+      user.pm("New #{status} map by #{mapper.username}: https://osu.ppy.sh/b/#{map['beatmap_id']}")
     rescue
       puts("Sending to #{user.username} for #{mapper.username}'s map failed.")
     else
@@ -18,18 +19,26 @@ def notify(map, mapper)
 end
 
 if __FILE__ == $0
+  now = DateTime.now
   puts("#{now.year}-#{now.month}-#{now.day} #{now.hour}:#{now.minute}")
   BOT = setup
-  JSON.load(HTTParty.get(SEARCH_URL).parsed_response)['beatmaps'].each do |map|
-    mapper_name = map['mapper']
-      result = DB.exec("SELECT * FROM mappers WHERE mapper_name = '#{mapper_name}'")
-      if result.ntuples > 0
-        mapper = Mapper.new(username: mapper_name)
-        if DB.exec("SELECT * FROM maps WHERE mapper_id = #{mapper.id} AND mapset_id = #{map['beatmapset']}").ntuples == 0
-
+  date = now - 1
+  date = "#{date.year}-#{date.month}-#{date.day} #{date.hour}:#{date.minute}"
+  url = "#{OSU_URL}/get_beatmaps?k=#{OSU_KEY}&since=#{date}"
+  mappers = []
+  HTTParty.get(url).parsed_response.each do |map|
+    mapper_name = map['creator']
+    mappers.include?(mapper_name) && next
+    mappers.push(mapper_name)
+    puts("Mapper: #{mapper_name}.")
+    result = DB.exec("SELECT * FROM mappers WHERE mapper_name = '#{mapper_name}'")
+    if result.ntuples > 0
+      mapper = Mapper.new(username: mapper_name)
+      puts("Mapper ID: #{mapper.id}.")
+      if DB.exec("SELECT * FROM maps WHERE mapper_id = #{mapper.id} AND mapset_id = #{map['beatmapset_id']}").ntuples == 0
         notify(map, mapper)
-        puts("Inserting (#{mapper.id}, #{map['beatmapset']}).")
-        DB.exec("INSERT INTO maps(mapper_id, mapset_id) VALUES (#{mapper.id}, #{map['beatmapset']})")
+        puts("Inserting (#{mapper.id}, #{map['beatmapset_id']}).")
+        DB.exec("INSERT INTO maps(mapper_id, mapset_id) VALUES (#{mapper.id}, #{map['beatmapset_id']})")
       end
     end
   end
